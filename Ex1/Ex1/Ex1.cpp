3c6ef06a6c56a174612b150f06ce8f8c3d884434
André Reis <andrereis2296@gmail.com>
2016-05-31 08:03:39 +0100

    Hello Word

---
+++ Ex1/Ex1/Ex1.cpp
@@ -0,0 +1,15 @@
+// Ex1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World\n";
+
+	system("Pause");
+
+    return 0;
+}
+
